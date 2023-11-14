d2ac0a81bbb257e8cbe71e771fb4a0d52b37dc28
renxiangsan <renxiangsan666@163.com>
2023-11-14 09:17:14 +0800

    *解决27号问题

--- rzmj/leetcode/27.js
+++ rzmj/leetcode/27.js
@@ -4,5 +4,23 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-  
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      for (let j = i; j < nums.length; j++) {
+        nums[j] = nums[j + 1]
+      }
+      i--
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0 || nums[i]) {
+
+    } else {
+      nums.pop()
+      i--
+    }
+  }
+  return nums.length
 };
+
+// console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));

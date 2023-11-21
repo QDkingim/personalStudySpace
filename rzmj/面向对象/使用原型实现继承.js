2ecaf3509c7e896f6be772c72e54f53f241bf1fb
QDkingim <renxiangsan666@163.com>
2023-11-21 21:23:32 +0800

    +使用原型实现继承

---
+++ rzmj/面向对象/使用原型实现继承.js
@@ -0,0 +1,15 @@
+function Person() {}
+Person.prototype.dance = function () {};
+
+function Ninja() {}
+
+// 重要的一步,一个对象的原型直接是另一个对象的实例
+
+Ninja.prototype = new Person();
+
+let ninja = new Ninja();
+
+console.log(ninja instanceof Ninja);
+console.log(ninja instanceof Person);
+console.log(ninja instanceof Object);
+console.log(typeof ninja.dance === "function");

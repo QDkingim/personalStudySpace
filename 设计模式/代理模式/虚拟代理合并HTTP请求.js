97330dada4ef9ee29c3a866f0662bf41eaab8757
renxiangsan <renxiangsan666@163.com>
2024-01-28 10:36:46 +0800

    +虚拟代理合并HTTP请求

---
+++ 设计模式/代理模式/虚拟代理合并HTTP请求.js
@@ -0,0 +1,31 @@
+var synchronousFile = function (id) {
+  console.log('开始同步文件,id为:' + id);
+}
+
+var proxySynchronousFile = function () {
+  var cache = [], // 保存一段时间内需要同步的ID
+    timer // 定时器
+  return function (id) {
+    cache.push(id);
+    if (timer) { // 保证不会覆盖已经启动的定时器
+      return
+    }
+
+    timer = setTimeout(function () {
+      synchronousFile(cache.join(',')) // 2秒后向本体发送需要同步的ID集合
+      clearTimeout(timer) // 清除定时器
+      timer = null
+      cache.length = 0 // 清空ID集合
+    }, 2000)
+  }
+}
+
+var checkbox = document.getElementsByTagName('input')
+
+for (let i = 0, c; c = checkbox[i++];) {
+  c.onclick = function () {
+    if (this.checked === true) {
+      proxySynchronousFile(this.id)
+    }
+  }
+}

33a553e8fa78f0d007b24fac1c6ef452a3c8ad03
https://gitee.com <renxiangsan666@163.com>
2024-08-16 16:50:01 +0800

    +轮转数组

--- leetcode/189. 轮转数组.js
+++ leetcode/189. 轮转数组.js
@@ -4,15 +4,21 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-  let tempNums = nums
-  let len = nums.length
-  for (let index = 0; index < k; index++) {
-    let t = nums[len - 1]
-    for (let i = len - 1; i >= 0; i--) {
-      tempNums[i - 1] = tempNums[i]
+  function reverse(i, j) {
+    while (i < j) {
+      [nums[i], nums[j]] = [nums[j], nums[i]]
+      i++
+      j--
     }
-    tempNums[0] = t
   }
 
-  console.log(tempNums);
+  let n = nums.length
+  k = k % n
+  reverse(0, n - 1)
+  reverse(0, k - 1)
+  reverse(k, n - 1)
 };
+
+
+
+// console.log();
